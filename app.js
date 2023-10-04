79992d64eab75731366f92daac728fb949971f2b
vii-ni <gvinicius105@gmail.com>
2023-10-04 14:43:19 -0300

    upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const favicon = require('serve-favicon')
 const cors = require('cors')
 const morgan = require('morgan')
+const favicon = require('serve-favicon')
 const path = require('path') 
 
 //database 
@@ -9,7 +9,6 @@ const {create_table, insertuser, selectuser, updateuser, deleteuser} = require('
 
 const PORT = 5000
 
-create_table()
 
 const app = express()
 //middlewares
@@ -17,7 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(morgan('common'))
 app.use(cors())
-
+app.use(favicon(path.join(__dirname,'static','touro.svg')))
 app.use(express.static('./static/'))
 
 app.route('/login')
@@ -25,26 +24,23 @@ app.route('/login')
 		res.sendFile(__dirname+'/views/login.html')
 	})
 	.post((req,res)=>{
-		res.sendFile(__dirname+'/views/login.html')
-			if(req.body){
-				res.redirect('/')
-			}
+		console.log(req.body)
+		res.redirect('/')
 	})
 
-app.route('/cadastro')
+app.route('/signup')
 	.get((req,res)=>{
 		res.sendFile(__dirname+'/views/cadastro.html')
 	})
 	.post((req,res)=>{
-		res.sendFile(__dirname+'/views/cadastro.html')
-			if (req.body){
-				res.redirect('/login')
-			}
+		console.log(req.body)
+		res.redirect('/login')
 	})
 
-app.get('/',(req,res)=>{
-	res.send('login feito')
-})
+app.route('/')
+	.get((req,res)=>{
+		res.sendFile(__dirname+'/views/home.html')
+	})
 
 app.listen(PORT,()=>{
 	console.log(`running in port:${PORT}`)
